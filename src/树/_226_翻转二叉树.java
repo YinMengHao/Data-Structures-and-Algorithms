408592a61a6250bfa37ca4b4a1000027aadd120d
MH <729263080@qq.com>
2023-05-12 08:33:35 +0800

    反转二叉树

--- src/树/_226_翻转二叉树.java
+++ src/树/_226_翻转二叉树.java
@@ -1,5 +1,25 @@
 package 树;
 
-public class _226_翻转二叉树 {
+import java.util.Queue;
 
+/**
+ * @author MH
+ * https://leetcode.cn/problems/invert-binary-tree/
+ */
+public class _226_翻转二叉树 {
+	public TreeNode invertTree(TreeNode root) {
+		preorderTraversal(root);
+		return root;
+    }
+	
+	public void preorderTraversal(TreeNode node) {
+		if (node == null) return;
+		TreeNode tempNode = node.left;
+		node.left = node.right;
+		node.right = tempNode;
+		
+		preorderTraversal(node.left);
+		preorderTraversal(node.right);
+		
+	}
 }
